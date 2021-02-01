9c10cecc1f1adab2e725a1418e4b956775a05ca3
Raphael Santana <raphaelstn@gmail.com>
2021-02-01 15:07:36 -0300

    sobreescrevendo actions padroes

--- projeto-pontos-turisticos/core/api/viewsets.py
+++ projeto-pontos-turisticos/core/api/viewsets.py
@@ -1,8 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
+
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(aprovado=True)
+
+    # def list(self, request, *args, **kwargs):
+    #     return Response({'Ok': 123})
+
+    # def create(self, request, *args, **kwargs):
+    #     return Response({'Hello': request.data['nome']})
+
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
+
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+
+    # def update(self, request, *args, **kwargs):
+    #     pass
+
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
