bd4ed6b679ffbc61242602c67e42e4bfc41eaea2
Raphael Santana <raphaelstn@gmail.com>
2021-02-01 15:13:43 -0300

    implementar metodos customizados

--- projeto-pontos-turisticos/core/api/viewsets.py
+++ projeto-pontos-turisticos/core/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -28,3 +29,12 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     # def partial_update(self, request, *args, **kwargs):
     #     pass
+
+    # necessita mandar PK por causa do detail True
+    @action(methods=['post'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def importar(self, request):
+        pass
