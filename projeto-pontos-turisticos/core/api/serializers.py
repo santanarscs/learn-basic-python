5b87c0a2f8ecc84be774f855e26098ea164d6ed2
Raphael Santana <raphaelstn@gmail.com>
2021-02-02 15:06:30 -0300

    salvando one to many nested relationship

--- projeto-pontos-turisticos/core/api/serializers.py
+++ projeto-pontos-turisticos/core/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from core.models import PontoTuristico
 from atracoes.models import Atracao
+from enderecos.models import Endereco
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 
@@ -34,8 +35,18 @@ class PontoTuristicoSerializer(ModelSerializer):
     def create(self, validated_data):
         atracoes = validated_data['atracoes']
         del validated_data['atracoes']
+
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+
         return ponto
 
     def get_descricao_completa(self, obj):
