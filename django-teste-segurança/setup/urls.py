df7dfbe321346e13821efc31dedbcbb808ec9143
Raphael Santana <raphaelstn@gmail.com>
2021-02-10 10:49:34 -0300

    configurando upload de imagens

---
+++ django-teste-segurança/setup/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls) ),
+    path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
+    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
+] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
